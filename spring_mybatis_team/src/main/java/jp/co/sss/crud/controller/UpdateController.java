c84573b2c0033baa540c6c6b84da0c93145778be
yarai <y_arai@braingate.co.jp>
2025-10-23 11:47:23 +0900

    add:変更機能の実装

--- spring_mybatis_team/src/main/java/jp/co/sss/crud/controller/UpdateController.java
+++ spring_mybatis_team/src/main/java/jp/co/sss/crud/controller/UpdateController.java
@@ -1,8 +1,5 @@
 package jp.co.sss.crud.controller;
 
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +8,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jp.co.sss.crud.entity.Department;
+import jp.co.sss.crud.entity.Employee;
 import jp.co.sss.crud.form.EmployeeForm;
+import jp.co.sss.crud.mapper.DepartmentMapper;
+import jp.co.sss.crud.mapper.EmployeeMapper;
 import jp.co.sss.crud.service.SearchForDepartmentByDeptIdService;
 import jp.co.sss.crud.service.SearchForEmployeesByEmpIdService;
 import jp.co.sss.crud.service.UpdateEmployeeService;
+import jp.co.sss.crud.util.BeanCopy;
 
 /**
  * 社員更新コントローラー
@@ -39,6 +43,18 @@ public class UpdateController {
 	 */
 	@Autowired
 	SearchForDepartmentByDeptIdService searchForDepartmentByDeptIdService;
+	
+	/**
+	 * Employeeテーブル用のマッパー
+	 */
+	@Autowired
+	EmployeeMapper employeeMapper;
+	
+	/**
+	 * Departmentテーブル用のマッパー
+	 */
+	@Autowired
+	DepartmentMapper departmentMapper;
 
 	/**
 	 * 社員情報の変更内容入力画面を出力
@@ -51,12 +67,8 @@ public class UpdateController {
 	 */
 	@RequestMapping(path = "/update/input", method = RequestMethod.GET)
 	public String inputUpdate(Integer empId, @ModelAttribute EmployeeForm employeeForm) {
-
-		//TODO 社員IDに紐づく社員情報を検索し、Employee型の変数に代入する
-
-		//TODO 検索した社員情報をformに積め直す(BeanCopyクラスを用いてもよい)	
-
-		// 更新確認画面のビュー名を返す
+		Employee employee = employeeMapper.findByEmpId(empId);
+		BeanCopy.copyEntityToForm(employee, employeeForm);
 		return "update/update_input";
 	}
 
@@ -71,16 +83,11 @@ public class UpdateController {
 	 */
 	@RequestMapping(path = "/update/check", method = RequestMethod.POST)
 	public String checkUpdate(@Valid @ModelAttribute EmployeeForm employeeForm, BindingResult result, Model model) {
-		// TODO 入力チェックでエラーが発生した場合
-		if (false) {
-			// エラーがある場合は入力画面に戻る
+		if (result.hasErrors()) {
 			return "update/update_input";
 		} else {
-			// TODO 部署IDから部署情報を検索する
-
-			// TODO 部署名をモデルに追加する
-
-			// 更新確認画面のビュー名を返す
+			Department department = departmentMapper.findByDeptId(employeeForm.getDeptId());
+			model.addAttribute("deptName", department.getDeptName());
 			return "update/update_check";
 		}
 	}
@@ -93,7 +100,6 @@ public class UpdateController {
 	 */
 	@RequestMapping(path = "/update/back", method = RequestMethod.POST)
 	public String backInputUpdate(@ModelAttribute EmployeeForm employeeForm) {
-		//  更新入力画面のビュー名を返す
 		return "update/update_input";
 	}
 
@@ -106,21 +112,18 @@ public class UpdateController {
 	 */
 	@RequestMapping(path = "/update/complete", method = RequestMethod.POST)
 	public String completeUpdate(EmployeeForm employeeForm, HttpSession session) {
-
-		// TODO フォームの内容をEmployeeエンティティにコピー
-
-		// TODO 権限がnullの場合、デフォルトの権限を設定
-		if (false) {
+		Employee employee = BeanCopy.copyFormToEmployee(employeeForm);
+		
+		if (employee.getAuthority() == null) {
+			employee.setAuthority(1);
 		}
-
-		// TODO 社員情報を更新する
-
-		// TODO セッションからユーザー情報を取得
-
-		// TODO ログイン中のユーザーが自分の情報を更新した場合、セッション情報も更新
-		if (false) {
-			// TODO セッションに保存されているユーザーの社員名を更新
-
+		
+		employeeMapper.update(employee);
+		Employee user = (Employee) session.getAttribute("user");
+		
+		if (employee.getEmpId() == user.getEmpId()) {
+			user.setEmpName(employee.getEmpName());
+			session.setAttribute("user", user);
 		}
 
 		// 更新完了画面へリダイレクト
