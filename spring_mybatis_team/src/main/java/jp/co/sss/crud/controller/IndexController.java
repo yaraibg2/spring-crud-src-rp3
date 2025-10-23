9d41485221af244d39332b78d1e728fb48fa51d7
yarai <y_arai@braingate.co.jp>
2025-10-23 09:28:12 +0900

    fix:test

--- spring_mybatis_team/src/main/java/jp/co/sss/crud/controller/IndexController.java
+++ spring_mybatis_team/src/main/java/jp/co/sss/crud/controller/IndexController.java
@@ -1,6 +1,5 @@
 package jp.co.sss.crud.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
